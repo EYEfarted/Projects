f21f22c2c2d2a975e31f9fff085c8cf857e27d4b
EYEfarted <tom.a.ireland@gmail.com>
2016-05-07 00:36:21 -0400

    Update battleship.py
    random location generator subbed in board size variables for restriants.. selection > vertical col > horiz row

--- battleship.py
+++ battleship.py
@@ -21,25 +21,25 @@ print_board(board)
 
 # program generated "ship" location
 def random_row(board):
-    return randint(0, len(board[0])-1)
+    return randint(0, y-1)
 
 def random_col(board):
-    return randint(0, len(board[0])-1)
+    return randint(0, x-1)
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-# print ship_row, ship_col
+print ship_col, ship_row
 
 # Game Loop
 for turn in range(4):
     print "Turn", turn + 1
-    guess_row = int(raw_input("Guess Row:"))
     guess_col = int(raw_input("Guess Column:"))
+    guess_row = int(raw_input("Guess Row:"))
     if guess_row == ship_row and guess_col == ship_col:
        print "Congratulations! You sank my battleship!", "Game Over"
        break
     else:
-        if guess_row not in range(0, x) or guess_col not in range(0, y):
+        if guess_row not in range(0, y) or guess_col not in range(0, x):
             print "Oops, that's not even in the ocean."
             print_board(board)
         elif board[guess_row][guess_col] == "X":
