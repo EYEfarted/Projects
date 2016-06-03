81a194884d85b957ab1a565616397f7aacbfb673
EYEfarted <tom.a.ireland@gmail.com>
2016-06-03 04:33:52 -0400

    Update find_in_permutation.py
    line 46
    added "and ch not in abc_letters"
    works for all now
    removing print statements

--- find_in_permutation.py
+++ find_in_permutation.py
@@ -5,8 +5,6 @@
 ## QUESTION is located at 24572
 ## BOOKKEEPER = 10743
 ## NONINTUITIVENESS = 8222334634
-
-
 def find_in_permutation(word):
     from math import factorial
 
@@ -17,36 +15,34 @@ def find_in_permutation(word):
     set_letters = list(set(letters))
     sorted_set = sorted(set_letters)
 
-    print letters
-    print set_letters
-
     def dom():
         dom = 1
         for ch in set_letters:
-            if word.count(ch)>1:
-                dom *= factorial(word.count(ch))
+            if abc_letters.count(ch)>1:
+            #    print ch,abc_letters.count(ch)
+                dom *= factorial(abc_letters.count(ch))
         return dom
+
 
     def top():
         return factorial(l)
 
     location = 1
     for ch in letters:
+        d = dom()
+
         l = max(1,len(abc_letters)-1)
-        print abc_letters
-        print "l =    ",l
-        print "top() =",top()
-        print "dom() =",dom()
+
         f = top()/dom()
         a = abc_letters.index(ch)
-        print "top()/dom() =",f
-        print "* abc_letters.index(ch)",f*a
+
         abc_letters.pop(abc_letters.index(ch))
         location += f*a
-        print "location= ",location
-        if ch in set_letters:
+
+        if ch in set_letters and ch not in abc_letters:
             set_letters.pop(set_letters.index(ch))
         else:
             continue
     return location
-find_in_permutation("BOOKKEEPER")
+
+find_in_permutation("NONINTUITIVENESS")
