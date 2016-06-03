c5ae6164d8b12819e137b2081eb66b24727da8b6
EYEfarted <tom.a.ireland@gmail.com>
2016-06-03 04:04:40 -0400

    Update find_in_permutation.py
    rethought math so that now factorial(len(word-1))/factorial(num duplicate letter)
    works for all examples except "NONINTUITIVENESS"

--- find_in_permutation.py
+++ find_in_permutation.py
@@ -6,61 +6,47 @@
 ## BOOKKEEPER = 10743
 ## NONINTUITIVENESS = 8222334634
 
-def big_loop(word):
-    from math import factorial as f
 
-    original_order_list = []
+def find_in_permutation(word):
+    from math import factorial
+
+    letters = []
     for char in word:
-        original_order_list.append(char)
+        letters.append(char)
+    abc_letters = sorted(letters)
+    set_letters = list(set(letters))
+    sorted_set = sorted(set_letters)
 
-    set_letters = sorted(list(set(original_order_list)))
-    sort_letters = sorted(original_order_list)
-    L = len(sort_letters)
-    location = 0
+    print letters
+    print set_letters
 
-    def find_equal(location):
-        a = 0
-        l = (max((len(set_letters)-1),0))
+    def dom():
+        dom = 1
         for ch in set_letters:
+            if word.count(ch)>1:
+                dom *= factorial(word.count(ch))
+        return dom
 
-            for x in sort_letters:
-                if set_letters == original_order_list and len(original_order_list) ==1:
-                    print "only one way to arrange this"
-                    location +=1
+    def top():
+        return factorial(l)
 
-                if x != original_order_list[a]:
-                    print x,"NOT equal",original_order_list[a]
-                    print "adding f(l) %s to location" % f(l)
-                    location = location + f(l)
-                    print "location =",location
-                else:
-                    print x,"EQUAL to",original_order_list[a]
-                    l = l - 1
-                    if len(sort_letters)>0:
-                        print original_order_list,"original_order_list"
-                        print sort_letters,"sort_letters"
-                        print set_letters,"set_letters"
-                        print "removing %s from sort_letters, original_order_list" % x
-                        original_order_list.pop(original_order_list.index(x))
-                        sort_letters.pop(sort_letters.index(x))
-                        if x in set_letters and x not in original_order_list:
-                            set_letters.pop(set_letters.index(x))
-                        print original_order_list,"original_order_list"
-                        print sort_letters,"sorted list"
-                        print set_letters,"set_letters"
-                        print "location =",location
-                    return sort_letters, location
-
-
-    def loop_thru_word():
-        for char in original_order_list:
-            loc = 0
-            sort_letters = []
-            for a in xrange(len(word)):
-                sort_letters, loc = find_equal(loc)
-            print "Sequence located at index: %s of permutation" % loc
-            return loc
-    return loop_thru_word()
-
-
-big_loop("BOOKKEEPER")
+    location = 1
+    for ch in letters:
+        l = max(1,len(abc_letters)-1)
+        print abc_letters
+        print "l =    ",l
+        print "top() =",top()
+        print "dom() =",dom()
+        f = top()/dom()
+        a = abc_letters.index(ch)
+        print "top()/dom() =",f
+        print "* abc_letters.index(ch)",f*a
+        abc_letters.pop(abc_letters.index(ch))
+        location += f*a
+        print "location= ",location
+        if ch in set_letters:
+            set_letters.pop(set_letters.index(ch))
+        else:
+            continue
+    return location
+find_in_permutation("BOOKKEEPER")
